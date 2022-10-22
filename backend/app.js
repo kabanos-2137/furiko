7d363994a836f9f5d2d50a462744eb66f21b0f92
kabanos-2137 <tvoastara251@gmail.com>
2022-10-22 22:36:20 +0200

    Create a login request

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,24 @@ app.post('/test', (req, res) => { //Test database
 	res.send(database.data); //Send database data
 });
 
+app.post('/login', (req, res) => { //Login Request
+	//Data sent by user
+	let _username = req.body.username;
+	let _password = req.body.password;
+
+	let _query = database.data.users.filter(el => //Find the user in db
+		el.username == _username && //Check if username is valid
+		el.password == _password //Check if password is valid
+	);
+
+	let _valid = _query.length > 0
+
+	res.send({
+		correct: (_valid ? true : false),
+		id: (_valid ? _query[0].id : undefined)
+	}); //Check if there are any results of the query and on this basis send data to user
+});
+
 app.listen(port, () => {
 	console.log(`App is running on port ${port}`)
 }) //Start app
