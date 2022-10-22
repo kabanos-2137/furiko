764bc3dcd93af04ce05c5c5fe09efe788402f0d8
kabanos-2137 <tvoastara251@gmail.com>
2022-10-22 23:19:33 +0200

    Create a get devices request

--- backend/app.js
+++ backend/app.js
@@ -42,6 +42,45 @@ app.post('/login', (req, res) => { //Login Request
 	}); //Check if there are any results of the query and on this basis send data to user
 });
 
+app.post('/get_devices', (req, res) => {
+	//Data sent by user
+	let _userId = req.body.userId;
+	let _password = req.body.password;
+
+	let _userQuery = database.data.users.filter(el => //Find the user in db
+		el.password == _password && //Check if password is valid
+		el.id == _userId //Check if user id is valid
+	);
+	let _devicesQuery = [] //Find the devices
+	let _devices = [] //Filter the devices data
+
+	if(_userQuery.length > 0){
+		_devicesQuery = database.data.devices.filter(el => //Find the devices
+			el.permissions.filter(el => 
+				el.userId == _userId && //Check if user id is valid
+				(el.type == "admin" || 
+				el.type == "user") //Check users permissions
+			).length > 0 //Check if user has permissions to this device
+		);
+		
+		if(_devicesQuery.length > 0){
+			_devicesQuery.forEach(el => {
+				_devices.push({ //Filter the devices data
+					id: el.id,
+					name: el.settings.name
+				});
+			});
+		}
+	}
+
+	let _valid = _userQuery.length > 0 && _devices.length > 0
+
+	res.send({
+		correct: (_valid ? true : false),
+		devices: (_valid ? _devices : undefined)
+	}); // Check if there are any results of the query and on this basis send data to user
+})
+
 app.listen(port, () => {
 	console.log(`App is running on port ${port}`)
 }) //Start app
